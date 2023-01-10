e1f47309415785ced63e0872e011b1dff59829d5
PrajaktaShewale <shewalep94@gmail.com>
2023-01-10 23:12:35 +0530

    [Prajakta]Add:Program to Add multiple Contacts

---
+++ AddressBook/AddressBookMain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+
+namespace AddressBook
+{
+    public class AddressBookMain
+    {
+        public List<Contact> Contacts = new List<Contact>();
+
+        public void CreateContact()
+        {
+            WriteLine("----Add new contact----");
+            WriteLine("First Name : ");string Fname=ReadLine();
+            WriteLine("Last Name : ");string Lname =ReadLine();
+            WriteLine("Address : ");string address =ReadLine();
+            WriteLine("City : ");string city =ReadLine();
+            WriteLine("State : ");string state =ReadLine();
+            WriteLine("ZipCode : ");int zip =Convert.ToInt32(ReadLine());
+            WriteLine("Phone Number : ");long phonenumber =Convert.ToInt64(ReadLine());
+            WriteLine("Email : ");string email =ReadLine();
+
+            Contact contact = new Contact(Fname, Lname, address, city, state, zip, phonenumber, email);
+            Contacts.Add(contact);
+        }
+
+        public void DisplayContact()
+        {
+            foreach(Contact contact in Contacts)
+            {
+                WriteLine("Name : {0} {1}",contact.FirstName,contact.LastName);
+                WriteLine("Address : {0} {1} {2} {3} ", contact.Address, contact.City, contact.State, contact.ZipCode);
+                WriteLine("Phone Number : {0}", contact.PhoneNumber);
+                WriteLine("Email : {0}", contact.Email);
+                WriteLine("*--------------********-----------*");
+            }
+            
+           
+        }
+    }
+}
---
+++ AddressBook/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("#------Welcome to Address Book Program------#");
+            Console.ResetColor();
+
+            AddressBookMain book = new AddressBookMain();
+            bool addcontact = true;            
+            while (addcontact)
+            {
+                book.CreateContact();
+                Console.WriteLine("Do you want to add more contact?(Y/N) :");
+                string ans = Console.ReadLine();
+                if(ans =="N" || ans == "n") { addcontact = false; }
+            }
+            book.DisplayContact();
+        }
+    }
+}
